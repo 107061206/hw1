018334c0e8d1b017be9a5978df06aa434bbf9b37
107061206 <cyhuang1129@gmail.com>
2020-03-17 22:21:36 +0800

    first commit

---
+++ hw1.py
@@ -0,0 +1,41 @@
+
+
+import csv                                                          
+
+
+cwb_filename = '107061206.csv'                                      
+data = []
+header = []
+with open(cwb_filename) as csvfile:                                 
+    mycsv = csv.DictReader(csvfile)
+    header = mycsv.fieldnames
+    for row in mycsv:
+        data.append(row) 
+
+
+    sum = list(range(5))    
+    out = list(range(5))
+    stationid = list(range(5))
+
+    stationid = ['C0A880', 'C0F9A0', 'C0G640', 'C0R190', 'C0X260']    
+
+    for i in range(5):                                              
+        sum[i] = 0
+        for row in data :
+            if row['station_id'] == stationid[i] :                     
+                if row['HUMD'] == '-99.000' or row['HUMD'] == '-999.000' :
+                    sum[i] = 0
+
+                else :    
+                    sum[i] += float(row['HUMD'])
+    
+    for i in range(5): 
+        if sum[i] == 0 :      
+            out[i] = [stationid[i], 'None']                          
+        else :
+            out[i] = [stationid[i], sum[i]]
+
+
+    print(out) 
+     
+
